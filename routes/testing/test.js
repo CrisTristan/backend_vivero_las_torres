a316363ec4827fe5eef413314d374fbd767cd416
Cristian <cristian.abad700@gmail.com>
2026-05-22 19:41:02 -0500

    se modifico la ruta de testing

--- routes/testing/test.js
+++ routes/testing/test.js
@@ -6,10 +6,10 @@ const router = Router();
 router.get("/test", async (req, res) => {
     try {
         
-        const orderController = new OrderController(null, null, null, null, null, null);
-        const newOrder = await orderController.NotifyAdminsOfNewOrder(123); // Aquí puedes pasar los detalles de la orden que quieras probar
+        //const orderController = new OrderController(null, null, null, null, null, null);
+        //const newOrder = await orderController.NotifyAdminsOfNewOrder(123); // Aquí puedes pasar los detalles de la orden que quieras probar
 
-        res.status(201).json({ message: "Orden creada exitosamente", administradores: newOrder });
+        res.status(201).json({ message: "Esto es una prueba de la ruta /test" });
     } catch (error) {
         console.error("Error al crear la orden:", error);
         res.status(500).json({ error: "Error al crear la orden" });
